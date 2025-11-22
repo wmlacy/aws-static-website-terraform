7c420661b16dbe2f8ea90627f81750f27fd81723
Will <wmlacy3000@gmail.com>
2025-11-22 10:51:26 +0200

    Initial Terraform skeleton with .gitignore

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 static site & CloudFront will go here in Phase 2+
---
+++ outputs.tf
@@ -0,0 +1,1 @@
+# Outputs will be added after we create resources
