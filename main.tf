58cd5b05ce05298e52fbf03b50bec02d7d84c986
Will <wmlacy3000@gmail.com>
2025-11-24 23:02:33 +0200

    Update website files and Terraform S3 config

--- main.tf
+++ main.tf
@@ -6,11 +6,106 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Portfolio = "CloudPortfolio"
+      Owner     = "Will"
+      ManagedBy = "Terraform"
+    }
+  }
 }
 
-# S3 static site & CloudFront will go here in Phase 2+
+resource "random_id" "rand" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket        = "wmlacy-static-site-${random_id.rand.hex}"
+  force_destroy = true
+
+  tags = {
+    Project     = "StaticWebsite"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.site.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.site.id
+  key          = "index.html"
+  source       = "${path.module}/index.html"
+  content_type = "text/html"
+  etag         = filemd5("${path.module}/index.html")
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket       = aws_s3_bucket.site.id
+  key          = "error.html"
+  source       = "${path.module}/error.html"
+  content_type = "text/html"
+  etag         = filemd5("${path.module}/error.html")
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "site_public" {
+  statement {
+    sid = "AllowPublicRead"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.site.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "site_public" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site_public.json
+
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,5 @@
+output "website_endpoint" {
+  description = "S3 static website endpoint"
+  value       = aws_s3_bucket_website_configuration.site.website_endpoint
+}
 # Outputs will be added after we create resources
